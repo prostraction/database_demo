51fc25e462ab9f15271ce96013f682e59583db25
prostraction <secret.darklight@github.com>
2022-04-11 00:58:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
         }
     });
 	$("#search_cpu").keyup(function() {
-		display_item("#display_cpu", '#search_cpu', "search_cpu");
+		display_item("#display_cpu", '#search_cpu');
 	});
 });
  
@@ -28,7 +28,7 @@ function fill(Key, Value, Display, Search) {
     $(Display).hide();
 }
 
-function display_item(Display, Search, Data) {
+function display_item(Display, Search) {
         var PostedValue = $(Search).val();
         if (PostedValue === "") {
             $(Display).html("");
@@ -38,7 +38,7 @@ function display_item(Display, Search, Data) {
                 type: "POST",
                 url: "handler.php",
                 data: {
-                    search_cpu: PostedValue
+                    $('Search): PostedValue
                 },
                 success: function(response) {
                     $(Display).html(response).show();
