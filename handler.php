a3a1e33d60bcf8cebc378edd5c4b65f76941e4c1
prostraction <secret.darklight@github.com>
2022-04-10 23:47:35 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -4,14 +4,14 @@ include "db.php";
 // if (isset($_POST['search_motherboard'])) {
 if (isset($_POST['search_motherboard'])) {
     $PostedValue = $_POST['search_motherboard'];
-	$requested_name = '#search_motherboard';
-    $Query = "SELECT model FROM cpu WHERE model LIKE '%$PostedValue%' LIMIT 5";
- 
+	$RequestedName = '#search_motherboard';
+    $Query = "SELECT model FROM motherboard WHERE model LIKE '%$PostedValue%' LIMIT 5";
     $ExecQuery = mysqli_query($connectionDB, $Query);
+	
     echo '<ul>';
     while ($Result = mysqli_fetch_array($ExecQuery)) {
 ?>
-        <li onclick='fill("<?php echo 'test'; ?>")'>
+        <li onclick='fill("<?php echo $Result['PostedValue']; ?>")'>
             <a>
                 <?php echo $Result[0]; ?>
             </a>
