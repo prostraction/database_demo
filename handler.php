d5ab2df8b4864a23d01e11f8b66102ba8f6d0d0c
prostraction <secret.darklight@github.com>
2022-04-11 15:08:19 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -83,7 +83,7 @@ if (isset($_POST['search_ram'])) {
 <?php
 if ($_POST['action'] == 'ram') {
 	$Argument = intval($_POST['argument_ram'], 10);
-	$Query = "INSERT INTO ram_computer (computer_id, ram_id) VALUES (999, %$Argument%)";
+	$Query = "INSERT INTO ram_computer (computer_id, 'ram_id') VALUES (999, '$Argument')";
 	$ExecQuery = mysqli_query($connectionDB, $Query);
 }
 
