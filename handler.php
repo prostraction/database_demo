ca5c1f1380708030c8250f3ba167e5ec32dfaa8e
prostraction <secret.darklight@github.com>
2022-04-11 19:34:49 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -105,8 +105,8 @@ if ($_POST['action'] == 'ram') {
 	//$_POST['argument_ram']
 	
 	$stmt = mysqli_prepare($connectionDB, "INSERT INTO ram_computer (computer_id, ram_id) VALUES (?, ?)");
-	$computer_id = "444";
-	$string_arg = "123456";
+	$computer_id = '444';
+	$string_arg = '123456';
 	$ram_id = $string_arg;//$_POST['argument_ram']);
 
 	mysqli_stmt_bind_param($stmt, "ss", $computer_id, $ram_id);
