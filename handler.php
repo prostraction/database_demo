4f8f421ecfb3a71b9db16e32bbf292ec51ec3201
prostraction <secret.darklight@github.com>
2022-04-11 18:17:50 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -86,8 +86,12 @@ if ($_POST['action'] == 'ram') {
 	
 	//$PassedArgument = intval($_POST['argument_ram'],10);
 	//$string1 = strval($PassedArgument);
-	//$Query = "INSERT INTO ram_computer (computer_id, ram_id) VALUES (300," . $ram_id . ");";	
-	$temp = 222;
+	//$Query = "INSERT INTO ram_computer (computer_id, ram_id) VALUES (300," . $ram_id . ");";
+	$temp = 111;
+	if ($_POST['argument_ram'] == 123456) {
+		$temp = 222;
+	}
+	
 	$Query = "INSERT INTO ram_computer (computer_id, ram_id) VALUES ('".$temp."', '".$temp."')";
 	$ExecQuery = mysqli_query($connectionDB, $Query);//sprintf($Query, 400, $PassedArgument));
 	//$stmt = mysqli_prepare($connectionDB, "INSERT INTO ram_computer (computer_id, ram_id) VALUES (?, ?)");
