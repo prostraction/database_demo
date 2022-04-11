c4108d3a6e5b75c8478810c44804de3c75d611bd
prostraction <secret.darklight@github.com>
2022-04-11 21:31:11 +0300

    Test

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function fill_multiple(Key, Value, Display, Search) {
             $.ajax({
                 type: "POST",
                 url: "handler.php",
-                data:{action:'ram', argument_ram:'123456'}
+                data:{action:'ram', argument_ram:'123'}
             });
         }
 }
