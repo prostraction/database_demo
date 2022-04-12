e3dc9a584ba4a0a81155c6e22d0ed449e7c2b005
prostraction <secret.darklight@github.com>
2022-04-12 22:48:09 +0300

    Test

--- handler.php
+++ handler.php
@@ -100,5 +100,9 @@ if ($_POST['action'] == 'ram_id_find') {
 	$ExecQuery = mysqli_query($connectionDB, $Query);
 	$get_ram_id = mysqli_fetch_array($ExecQuery);
 	
-
+	$stmt = mysqli_prepare($connectionDB, "INSERT INTO test (x) VALUES (?);");
+	$string_arg1 = $get_ram_id[0];
+	$computer_id = $string_arg1;
+	mysqli_stmt_bind_param($stmt, "s", $computer_id);
+	mysqli_stmt_execute($stmt);
 }
