5a8d1f43cbc0693bd8bb79b9f54108be0dcfce99
prostraction <secret.darklight@github.com>
2022-04-12 23:01:58 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -96,10 +96,14 @@ if ($_POST['action'] == 'ram') {
 <?php
 if ($_POST['action'] == 'ram_id_find') {
 	$model = $_POST['argument_ram_id'];
-	$Query = "SELECT id FROM ram WHERE model = '$model' LIMIT 1;";
-	$ExecQuery = mysqli_query($connectionDB, $Query);
+	$stmt = mysqli_prepare($connectionDB, "SELECT id FROM ram WHERE model=? LIMIT 1");
+	mysqli_stmt_bind_param($stmt, "s", $model);
+	mysqli_stmt_execute($stmt);
+	$get_ram_id = mysqli_stmt_get_result($stmt);
+	
+	//$ExecQuery = mysqli_query($connectionDB, $Query);
 	// $get_ram_id = mysqli_fetch_assoc($ExecQuery);
-	while ($get_ram_id = mysqli_fetch_array($ExecQuery)) { ; }
+	//while ($get_ram_id = mysqli_fetch_array($ExecQuery)) {}
 	
 	$stmt = mysqli_prepare($connectionDB, "INSERT IGNORE INTO test (x) VALUES (?);");
 	$string_arg1 = $get_ram_id[0];
