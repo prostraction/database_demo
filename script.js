67b969ff8e074bb159123a547cc3c962500acca5
prostraction <47314760+prostraction@users.noreply.github.com>
2022-04-13 03:26:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function display_inserted(Display, Search_Object) {
 	$.ajax({
         type: "POST",
         url: "handler.php",
-        data: Search_Object+'=111',
+        data: Search_Object+'='+Search_Object,
         success: function(response) {
         $(Display).html(response).show();
             }
