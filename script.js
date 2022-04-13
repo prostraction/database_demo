19de08cb93efe5b44bd9456499d4e75da735f922
prostraction <47314760+prostraction@users.noreply.github.com>
2022-04-13 03:24:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 	$("#search_motherboard").keyup(function() {
-		display_item("#display_motherboard", '#search_motherboard', "search_motherboard");
+		display_search("#display_motherboard", '#search_motherboard', "search_motherboard");
 	});
 	$("#search_cpu").keyup(function() {
-		display_item("#display_cpu", '#search_cpu', "search_cpu");
+		display_search("#display_cpu", '#search_cpu', "search_cpu");
 	});
 	$("#search_cpu_fan").keyup(function() {
-		display_item("#display_cpu_fan", '#search_cpu_fan', "search_cpu_fan");
+		display_search("#display_cpu_fan", '#search_cpu_fan', "search_cpu_fan");
 	});
 	$("#search_ram").keyup(function() {
-		//display_item("#display_ram", '#search_ram', "search_ram");
-		display_item("#test_ram", '#show_ram', "show_ram");
+		display_search("#display_ram", '#search_ram', "search_ram");
+		display_inserted("#test_ram", '#show_ram', "show_ram");
 		// TO DO: copy to autoload
 	});
 });
@@ -22,7 +22,7 @@ function fill(Key, Value, Display, Search) {
 }
 
 function fill_computer_ram(Key, ValueId, ValueName, Display, Search) {
-    //display_item("#test", '#search_ram', "show_ram");
+    display_inserted("#test_ram", '#show_ram', "show_ram");
 	var PostedValue = $(ValueName).val();
 	$(Key).text(ValueName);
 	$(Search).val('');
@@ -38,7 +38,7 @@ function fill_computer_ram(Key, ValueId, ValueName, Display, Search) {
     }
 }
 
-function display_item(Display, Search, Search_Object) {
+function display_search(Display, Search, Search_Object) {
         var PostedValue = $(Search).val();
         if (PostedValue === "") {
             $(Display).html("");
@@ -53,4 +53,16 @@ function display_item(Display, Search, Search_Object) {
                 }
             });
         }
+}
+
+function display_inserted(Display, Search_Object) {
+	$(Display).html("");
+	$.ajax({
+        type: "POST",
+        url: "handler.php",
+        data: Search_Object+'='+PostedValue,
+        success: function(response) {
+        $(Display).html(response).show();
+            }
+        });
 }
