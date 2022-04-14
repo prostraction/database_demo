5a33222b6a9d333a64b4c35b31f045243f149e2c
prostraction <47314760+prostraction@users.noreply.github.com>
2022-04-14 23:02:09 +0300

    Update db.php

--- db.php
+++ db.php
@@ -6,6 +6,9 @@
         "comp_conf"
     );
 	
+	$computer_id = 3;
+	$disk_id = 0;
+	
     if (mysqli_connect_errno()) {
         echo "Unable to connect to MySQL: " . mysqli_connect_error();
     }
