da37a605c5fceeeef1c8dcbafd3c48c0d61a306a
prostraction <47314760+prostraction@users.noreply.github.com>
2022-04-14 22:37:41 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,16 +1,16 @@
 <?php
 include "db.php";
 
-global $computer_id;
-global $disk_id;
+//global $computer_id;
+//global $disk_id;
 
 //$computer_id = 3;
 //$disk_id = 0;
 
 
 if ($_POST['configuration'] == 'new_configuration') {
-	global $computer_id;
-	global $disk_id;
+	//global $computer_id;
+	//global $disk_id;
 	$QueryDisk = "INSERT INTO disk (sata_count, m2_count) VALUES (0, 0)";
     $ExecQuery = mysqli_query($connectionDB, $QueryDisk);
 	$disk = mysqli_insert_id($connectionDB);
